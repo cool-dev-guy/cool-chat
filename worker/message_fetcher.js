5db51ff9cb858a330ea66e0fb415b09df713ef19
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2023-09-25 01:23:08 +0530

    Update message_fetcher.js

--- worker/message_fetcher.js
+++ worker/message_fetcher.js
@@ -1,9 +1,28 @@
-var i = 0;
+// listen for the url
+onmessage = function (event) {
+  var url = event.data;
+  console.log('[Web worker] Web worker ready');
+  makeRequest(url);
+};
 
-function timedCount() {
-  i = i + 1;
-  postMessage(i);
-  setTimeout("timedCount()",500);
+function makeRequest(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        postMessage(xhr.responseText);
+        setTimeout(function () {
+          makeRequest(url);
+        }, 500); // Retry after a 500 ms (0.5-second) delay.
+      } else {
+        // There was an error in the request.
+        console.error("Request error:", xhr.status, xhr.statusText);
+        setTimeout(function () {
+          makeRequest(url); // Retry the request after a delay.
+        }, 1000); // Retry after 1 second.
+      }
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
 }
-
-timedCount(); 
